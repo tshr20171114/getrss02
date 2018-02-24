a6f9a0088cb82b8bf64d95482b5aa3b3d6a61511
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-02-24 11:33:05 +0900

    Update jump.php

--- www/jump.php
+++ www/jump.php
@@ -1,6 +1,19 @@
 <?php
 
-$url = $_GET['u'];
+$jump_url = $_GET['u'];
 
-header('Location: ' . $url);
+$url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . "/tag/JUMP/";
+
+$context = [
+  'http' => [
+    'method' => 'POST',
+    'header' => [
+      'Content-Type: text/plain'
+    ],
+  'content' => $jump_url
+  ]];
+
+file_get_contents($url, false, stream_context_create($context));
+
+header('Location: ' . $jump_url);
 ?>
