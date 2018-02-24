8ed19c5297d18efba2702bc6cbbb85d5b3319cb5
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-02-25 04:16:08 +0900

    Update jump2.php

--- www/jump2.php
+++ www/jump2.php
@@ -14,5 +14,11 @@ error_log('TITLETAG ' . $jump_url . ' ' . $matches[1] . ' ' . $jump_url2);
 
 $res = file_get_contents($jump_url2);
 
-header('Location: ' . $jump_url2);
+$rc = preg_match(getenv('PATTERN1'), $res, $matches);
+
+$jump_url3 = $matches[1];
+
+error_log($jump_url3);
+
+//header('Location: ' . $jump_url2);
 ?>
