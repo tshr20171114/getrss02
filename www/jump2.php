7e175efb663f31e8c2572f0c99f99b767414df34
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-02-25 04:28:10 +0900

    Update jump2.php

--- www/jump2.php
+++ www/jump2.php
@@ -9,16 +9,18 @@ $title = $matches[1];
 
 $rc = preg_match('/<div class="gotoBlog"><a href="(.+?)"/', $res, $matches);
 $jump_url2 = $matches[1];
-
+ 
 error_log('TITLETAG ' . $jump_url . ' ' . $title . ' ' . $jump_url2);
 
 $res = file_get_contents($jump_url2);
 
 $rc = preg_match(getenv('PATTERN1'), $res, $matches);
 
-$jump_url3 = $matches[1];
-
-error_log($jump_url3);
-
-//header('Location: ' . $jump_url2);
+if ($rc === 1) {
+  $jump_url3 = $matches[1];
+  error_log($jump_url3);
+  header('Location: ' . $jump_url3);
+} else {
+  header('Location: ' . $jump_url2);
+}
 ?>
