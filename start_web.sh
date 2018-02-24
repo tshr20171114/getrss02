a32279ee582b0897ce4433ae251a0e6b2933ee1c
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-02-24 11:02:26 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -8,6 +8,9 @@ httpd -V
 httpd -M
 php --version
 whereis php
+cat /proc/version
+curl --version
+printenv
 
 if [ ! -v LOGGLY_TOKEN ]; then
   echo "Error : LOGGLY_TOKEN not defined."
@@ -28,6 +31,23 @@ if [ ! -v BASIC_PASSWORD ]; then
   exit
 fi
 
+if [ ! -v HOME_FQDN ]; then
+  echo "Error : HOME_FQDN not defined."
+  exit
+fi
+
+if [ ! -v HEROKU_APP_NAME ]; then
+  echo "Error : HEROKU_APP_NAME not defined."
+  exit
+fi
+
 htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
 
+nslookup ${HOME_FQDN} 8.8.8.8
+
+export HOME_IP_ADDRESS=$(nslookup ${HOME_FQDN} 8.8.8.8 \
+  | grep ^Address \
+  | grep -v 8.8.8.8 \
+  | awk '{print $2}')
+    
 vendor/bin/heroku-php-apache2 -C apache.conf www
