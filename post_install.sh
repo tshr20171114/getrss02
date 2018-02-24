c23085bc06c6dab6c65e72577cfd1caaddf128c7
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-02-25 05:24:17 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,13 +4,7 @@ set -x
 
 date
 
-tar xf phpPgAdmin-5.1.tar.bz2
 
-mv phpPgAdmin-5.1 www/phppgadmin
-
-rm -f phpPgAdmin-5.1.tar.bz2
-
-cp config.inc.php www/phppgadmin/conf/config.inc.php
 
 chmod 755 ./start_web.sh
 
