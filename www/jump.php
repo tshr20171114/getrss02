dec4ca78c5849724d3b006f7fa300773ed6f0a8d
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-02-24 11:50:31 +0900

    Update jump.php

--- www/jump.php
+++ www/jump.php
@@ -4,7 +4,7 @@ $jump_url = $_GET['u'];
 
 $res = file_get_contents($jump_url);
 
-$rc = preg_match('/<title>.+<.title>/', $res, $matches);
+$rc = preg_match('/<title>(.+)<.title>/', $res, $matches);
 
 error_log($jump_url);
 error_log($rc);
