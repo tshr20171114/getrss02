5f74fe0248d4c9d960f5399fd19fe79b8fa27afe
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-02-25 03:35:50 +0900

    Create jump2.php

---
+++ www/jump2.php
@@ -0,0 +1,15 @@
+<?php
+
+$jump_url = $_GET['u'];
+
+$res = file_get_contents($jump_url);
+
+$rc = preg_match('/<title>(.+)<.title>/', $res, $matches);
+$title = $matches[1];
+
+error_log($jump_url);
+error_log($rc);
+error_log('TITLETAG ' . $jump_url . ' ' . $matches[1]);
+
+header('Location: ' . $jump_url);
+?>
