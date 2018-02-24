cda49cbb06163d14d219a56301001f39fc09f7ee
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-02-25 04:08:32 +0900

    Update jump2.php

--- www/jump2.php
+++ www/jump2.php
@@ -7,14 +7,10 @@ $res = file_get_contents($jump_url);
 $rc = preg_match('/<title>(.+)<.title>/', $res, $matches);
 $title = $matches[1];
 
-error_log($jump_url);
-error_log($rc);
-error_log('TITLETAG ' . $jump_url . ' ' . $matches[1]);
-
 $rc = preg_match('/<div class="gotoBlog"><a href="(.+?)"/', $res, $matches);
 $jump_url2 = $matches[1];
 
-error_log($jump_url2);
+error_log('TITLETAG ' . $jump_url . ' ' . $matches[1] . ' ' . $jump_url2);
 
 header('Location: ' . $jump_url2);
 ?>
