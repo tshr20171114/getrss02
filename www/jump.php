76c4e5fca417302787e2252589b9d21c43129203
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-06-24 06:27:14 +0900

    Update jump.php

--- www/jump.php
+++ www/jump.php
@@ -2,7 +2,8 @@
 
 $jump_url = $_GET['u'];
 
-$res = file_get_contents($jump_url);
+// $res = file_get_contents($jump_url);
+$res = get_contents($jump_url);
 
 $rc = preg_match('/<title>(.+)<.title>/', $res, $matches);
 $title = $matches[1];
@@ -12,7 +13,8 @@ $jump_url2 = $matches[1];
  
 error_log('TITLETAG ' . $jump_url . ' ' . $title . ' ' . $jump_url2);
 
-$res = file_get_contents($jump_url2);
+// $res = file_get_contents($jump_url2);
+$res = get_contents($jump_url2);
 
 for ($i = 0; $i < 10; $i++) {
   if (getenv('PATTERN_B' . $i) !== FALSE) {
@@ -71,4 +73,27 @@ foreach ($patterns as $pattern) {
 }
 
 header('Location: ' . $jump_url2);
+
+exit();
+
+function get_contents($url_) {
+  $pid = getmypid();
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url_); 
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+  curl_setopt($ch, CURLOPT_ENCODING, "");
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
+  curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; rv:56.0) Gecko/20100101 Firefox/60.0');
+  // curl_setopt($ch, CURLINFO_HEADER_OUT, TRUE);
+  $contents = curl_exec($ch);
+  // error_log(curl_getinfo($ch, CURLINFO_HEADER_OUT));
+  // $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  // $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+  
+  curl_close($ch);
+  
+  return $contents;
+}
 ?>
