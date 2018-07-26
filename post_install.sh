908f6fb457a50d188fc6d47435c1e95c07fc5724
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-27 05:09:57 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -38,6 +38,14 @@ CREATE TABLE m_pattern (
 __HEREDOC__
 cat /tmp/sql_result.txt
 
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+CREATE TABLE t_link (
+ url character varying(512) primary key
+,create_time timestamp DEFAULT localtimestamp NOT NULL
+);
+__HEREDOC__
+cat /tmp/sql_result.txt
+
 # ***** phppgadmin *****
 
 pushd www
