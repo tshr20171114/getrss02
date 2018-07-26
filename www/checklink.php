449fd7bef314d340b2520f8fa08ffb50a6f278c3
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-27 05:28:40 +0900

    Update checklink.php

--- www/checklink.php
+++ www/checklink.php
@@ -38,11 +38,27 @@ for ($i = 0; $i < 2; $i++) {
   $url3 = str_replace($pattern[1], $matches[1], $pattern[2]);
 
   error_log("${pid} ${url3}");
-  error_log("${pid} " . urlencode($url3));
-  error_log("${pid} " . sha1($url3));
 }
 
-header('Content-Type: text/plain');
-echo  $url3;
 
+header('Content-Type: text/plain');
+
+$connection_info = parse_url(getenv('DATABASE_URL'));
+$pdo = new PDO(
+  "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
+  $connection_info['user'],
+  $connection_info['pass']);
+
+$sql = <<< __HEREDOC__
+SELECT 'DUMMY' FROM t_link WHERE uri = :b_uri
+__HEREDOC__;
+$statement = $pdo->prepare($sql);
+
+$statement_select->execute([':b_uri' => $url3 ]);
+$result = $statement_select->fetch();
+if ($result === FALSE) {
+  echo 'NONE';
+} else {
+  echo 'EXISTS';
+}
 ?>
