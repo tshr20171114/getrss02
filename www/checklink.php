2c4a3e61fc33753316b42681f6ce44d31bafe5d3
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-28 15:17:00 +0900

    Update checklink.php

--- www/checklink.php
+++ www/checklink.php
@@ -8,39 +8,13 @@ error_log("${pid} ${url}");
 
 header('Content-Type: text/plain');
 
-$res = get_contents($url);
-
-$rc = preg_match('/<div class="gotoBlog"><a href="(.+?)"/', $res, $matches);
-
-if ($rc != 1) {
-  echo 'NONE';
-  exit;
-}
-
-$url2 = $matches[1];
+$url2 = get_contents(getenv('CHECKURL') . $url);
 
 error_log("${pid} ${url2}");
 
-$res = get_contents($url2);
-
-$url3 = 'none';
-for ($i = 0; $i < 2; $i++) {
-  if ($i == 0) {
-     $pattern = explode(',', getenv('LINK_PATTERN1'));
-  } else {
-     $pattern = explode(',', getenv('LINK_PATTERN2'));
-  }
-  $rc = preg_match('/' . $pattern[0] . '/', $res, $matches);
-
-  if ($rc != 1) {
-    continue;
-  }
-
-  error_log("${pid} " . $matches[1]);
-
-  $url3 = str_replace($pattern[1], $matches[1], $pattern[2]);
-
-  error_log("${pid} ${url3}");
+if ($url2 === 'NONE') {
+  echo 'NONE';
+  exit;
 }
 
 $connection_info = parse_url(getenv('DATABASE_URL'));
@@ -54,7 +28,7 @@ SELECT 'DUMMY' FROM t_link WHERE uri = :b_uri
 __HEREDOC__;
 $statement = $pdo->prepare($sql);
 
-$statement->execute([':b_uri' => $url3 ]);
+$statement->execute([':b_uri' => $url2 ]);
 $result = $statement->fetch();
 if ($result === FALSE) {
   echo 'NONE';
@@ -80,7 +54,11 @@ function get_contents($url_) {
                      CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; rv:56.0) Gecko/20100101 Firefox/61.0',
                     ]);
   $contents = curl_exec($ch);
-  curl_close($ch);  
+  $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  curl_close($ch);
+  if ($http_code != '200') {
+    $contents = 'NONE';
+  }
   return $contents;
 }
 ?>
