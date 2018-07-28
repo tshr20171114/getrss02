179a29465144c624e0adccb2c99216af7a1f96cb
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2018-07-29 02:51:12 +0900

    Update jump.php

--- www/jump.php
+++ www/jump.php
@@ -2,7 +2,6 @@
 
 $jump_url = $_GET['u'];
 
-// $res = file_get_contents($jump_url);
 $res = get_contents($jump_url);
 
 $rc = preg_match('/<title>(.+)<.title>/', $res, $matches);
@@ -13,14 +12,7 @@ $jump_url2 = $matches[1];
  
 error_log('TITLETAG ' . $jump_url . ' ' . $title . ' ' . $jump_url2);
 
-// $res = file_get_contents($jump_url2);
 $res = get_contents($jump_url2);
-
-for ($i = 0; $i < 10; $i++) {
-  if (getenv('PATTERN_B' . $i) !== FALSE) {
-    $patterns_b[] = getenv('PATTERN_B' . $i);
-  }
-}
 
 $connection_info = parse_url(getenv('DATABASE_URL'));
 $pdo = new PDO(
@@ -33,37 +25,52 @@ INSERT INTO t_link ( uri ) VALUES ( :b_uri )
 __HEREDOC__;
 $statement = $pdo->prepare($sql);
 
-foreach ($patterns_b as $pattern) {
-  $ar = explode(',', $pattern);
-  $rc = preg_match($ar[0], $res, $matches);
-  if ($rc === 1) {
-    $base_url = $matches[1];
-    error_log('TEMP1 : ' . $base_url);
-    $embed_url = preg_replace($ar[1], $ar[2], $base_url);
-    error_log('TEMP2 : ' . $embed_url);
-    
-    echo '<HTML><HEAD>';
-    echo '<TITLE>' . $title . '</TITLE>';
-    echo '</HEAD><BODY>';
-    echo '<iframe src="';
-    echo $embed_url;
-    echo '" frameborder=0 width=100% height=480 scrolling=no></iframe>';
-    echo '</BODY></HTML>';
-   
-    $statement->execute([':b_uri' => $embed_url]);
-    $pdo = null;
-    exit();
-  }
-}
-
 $pattern = explode(',', getenv('LINK_PATTERN1'));
 $rc = preg_match('/' . $pattern[0] . '/', $res, $matches);
-if ($rc == 1) {
+if ($rc === 1) {
   $jump_url3 = str_replace($pattern[1], $matches[1], $pattern[2]);
   error_log($jump_url3);
   $statement->execute([':b_uri' => $jump_url3]);
   $pdo = null;
   header('Location: ' . $jump_url3);
+  exit();
+}
+
+$pattern = explode(',', getenv('LINK_PATTERN2'));
+$rc = preg_match('/' . $pattern[0] . '/', $res, $matches);
+if ($rc === 1) {
+  $embed_url = str_replace($pattern[1], $matches[1], $pattern[2]);
+  error_log($embed_url);
+  $statement->execute([':b_uri' => $embed_url]);
+  $pdo = null;
+  
+  echo '<HTML><HEAD>';
+  echo '<TITLE>' . $title . '</TITLE>';
+  echo '</HEAD><BODY>';
+  echo '<iframe src="';
+  echo $embed_url;
+  echo '" frameborder=0 width=100% height=480 scrolling=no></iframe>';
+  echo '</BODY></HTML>';
+ 
+  exit();
+}
+
+$pattern = explode(',', getenv('LINK_PATTERN3'));
+$rc = preg_match('/' . $pattern[0] . '/', $res, $matches);
+if ($rc === 1) {
+  $embed_url = str_replace($pattern[1], $matches[1], $pattern[2]);
+  error_log($embed_url);
+  $statement->execute([':b_uri' => $embed_url]);
+  $pdo = null;
+  
+  echo '<HTML><HEAD>';
+  echo '<TITLE>' . $title . '</TITLE>';
+  echo '</HEAD><BODY>';
+  echo '<iframe src="';
+  echo $embed_url;
+  echo '" frameborder=0 width=100% height=480 scrolling=no></iframe>';
+  echo '</BODY></HTML>';
+ 
   exit();
 }
 
@@ -74,7 +81,6 @@ header('Location: ' . $jump_url2);
 exit();
 
 function get_contents($url_) {
-  // $pid = getmypid();
   $ch = curl_init();
   curl_setopt_array($ch,
                     [CURLOPT_URL => $url_,
@@ -89,14 +95,8 @@ function get_contents($url_) {
                      CURLOPT_PATH_AS_IS => TRUE,
                      CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; rv:56.0) Gecko/20100101 Firefox/61.0',
                     ]);
-  // curl_setopt($ch, CURLINFO_HEADER_OUT, TRUE);
-  $contents = curl_exec($ch);
-  // error_log(curl_getinfo($ch, CURLINFO_HEADER_OUT));
-  // $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-  // $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-  
-  curl_close($ch);
-  
+  $contents = curl_exec($ch);  
+  curl_close($ch);  
   return $contents;
 }
 ?>
