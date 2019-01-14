16413194708d93320636da6422e8f4e343e8e6fc
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2019-01-15 01:07:31 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -3,5 +3,7 @@
 set -x
 
 export TZ=JST-9
-    
+
+export USER_AGENT=$(curl https://raw.githubusercontent.com/tshr20171114/getrss02/master/useragent.txt)
+
 vendor/bin/heroku-php-apache2 -C apache.conf www
