f550b1da8759a58e9776dbb8f45d891655e2de53
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2019-01-15 01:48:07 +0900

    Update rss01.php

--- www/rss01.php
+++ www/rss01.php
@@ -7,42 +7,52 @@ $mh = curl_multi_init();
 
 error_log(getenv('USER_AGENT'));
 
-for ($i = 0; $i < 30; $i++) {
-  $url = getenv('URL_010') . ($i + 1);
-  error_log($url);
+$list_res = [];
 
-  $ch = curl_init();
+for ($j = 0; $j < 2; $j++) {
+  for ($i = 0; $i < 30; $i++) {
+    $url = getenv('URL_010') . ($i + 1);
+    if (array_key_exists($url, $list_res)) {
+      continue;
+    }
+    error_log($url);
+
+    $ch = curl_init();
   
-  $options = [
-          CURLOPT_URL => $url,
-          CURLOPT_USERAGENT => getenv('USER_AGENT'),
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => '',
-          CURLOPT_FOLLOWLOCATION => 1,
-          CURLOPT_MAXREDIRS => 3,
-          CURLOPT_SSL_FALSESTART => true,
-  ];
-  curl_setopt_array($ch, $options);
+    $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_USERAGENT => getenv('USER_AGENT'),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_FOLLOWLOCATION => 1,
+            CURLOPT_MAXREDIRS => 3,
+            CURLOPT_SSL_FALSESTART => true,
+    ];
+    curl_setopt_array($ch, $options);
   
-  curl_multi_add_handle($mh, $ch);
-  $list_ch[$url] = $ch;
-}
-
-$active = null;
-$rc = curl_multi_exec($mh, $active);
-
-while ($active && $rc == CURLM_OK) {
-  if (curl_multi_select($mh) == -1) {
-      usleep(1);
+    curl_multi_add_handle($mh, $ch);
+    $list_ch[$url] = $ch;
   }
-  $rc = curl_multi_exec($mh, $active);
-}
 
-foreach ($list_ch as $url => $ch) {
-  $res = curl_getinfo($ch);
-  error_log($res['http_code'] . " ${url}");
-  if ($res['http_code'] == '200') {
-    $list_res[] = curl_multi_getcontent($ch);
+  $active = null;
+  $rc = curl_multi_exec($mh, $active);
+
+  while ($active && $rc == CURLM_OK) {
+    if (curl_multi_select($mh) == -1) {
+        usleep(1);
+    }
+    $rc = curl_multi_exec($mh, $active);
+  }
+
+  foreach ($list_ch as $url => $ch) {
+    $res = curl_getinfo($ch);
+    error_log($res['http_code'] . " ${url}");
+    if ($res['http_code'] == '200') {
+      $tmp = curl_multi_getcontent($ch);
+      if (strlen($tmp) > 100000) {
+        $list_res[$url] = curl_multi_getcontent($ch);
+      }
+    }
   }
 }
 
