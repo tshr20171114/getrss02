b0c8fa4e6ceb5532517272e4bd1f46f51ee28565
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2019-01-15 09:45:43 +0900

    Update rss01.php

--- www/rss01.php
+++ www/rss01.php
@@ -18,7 +18,7 @@ for ($j = 0; $j < 2; $j++) {
     error_log($url);
 
     $ch = curl_init();
-  
+
     $options = [
             CURLOPT_URL => $url,
             CURLOPT_USERAGENT => getenv('USER_AGENT'),
@@ -29,7 +29,7 @@ for ($j = 0; $j < 2; $j++) {
             CURLOPT_SSL_FALSESTART => true,
     ];
     curl_setopt_array($ch, $options);
-  
+
     curl_multi_add_handle($mh, $ch);
     $list_ch[$url] = $ch;
   }
@@ -49,7 +49,7 @@ for ($j = 0; $j < 2; $j++) {
     error_log($res['http_code'] . " ${url}");
     if ($res['http_code'] == '200') {
       $tmp = curl_multi_getcontent($ch);
-      error_log(strlen($tmp));
+      error_log('size : ' . strlen($tmp));
       if (strlen($tmp) > 100000) {
         $list_res[$url] = $tmp;
       }
@@ -60,35 +60,27 @@ for ($j = 0; $j < 2; $j++) {
   curl_multi_close($mh);
 }
 
+error_log('count : ' . count($list_res));
 foreach ($list_res as $res) {
-  error_log(strlen($res));
   $tmp1 = explode('<div class="innerHeaderSubMenu langTextSubMenu">', $res, 2);
   $tmp1 = explode('<div class="pagination3">', $tmp1[1]);
 
-  // error_log($tmp1[0]);
-
   $list = explode('</li>', $tmp1[0]);
 
-  // error_log(print_r($list, true));
-
   foreach ($list as $item) {
-    // error_log($item);
     $rc = preg_match('/data-thumb_url = "(.+?)"/s', $item, $match);
     if ($rc === 0) {
       continue;
     }
-    // error_log(print_r($match, true));
     $thumbnail = $match[1];
 
     $rc = preg_match('/<var class="duration">(.+?):/s', $item, $match);
-    // error_log(print_r($match, true));
     $time = (int)$match[1];
     if ($time < 50) {
       continue;
     }
 
     $rc = preg_match('/<a href="(.+?)".+?title="(.+?)"/s', $item, $match);
-    // error_log(print_r($match, true));
     $url_parts = parse_url($url);
     $link = $url_parts['scheme'] . '://' . $url_parts['host'] . $match[1];
     $title = $match[2];
@@ -115,26 +107,94 @@ error_log('file size : ' . $rc);
 
 $ftp_link_id = ftp_connect(getenv('FC2_FTP_SERVER'));
 $rc = ftp_login($ftp_link_id, getenv('FC2_FTP_ID'), getenv('FC2_FTP_PASSWORD'));
-
-error_log($rc);
+error_log('ftp_login : ' . $rc);
 
 $rc = ftp_pasv($ftp_link_id, true);
-
-error_log($rc);
+error_log('ftp_pasv : ' . $rc);
 
 $rc = ftp_nlist($ftp_link_id, '.');
-
 error_log(print_r($rc, true));
 
 $rc = ftp_put($ftp_link_id, getenv('RSS_010_FILE'), '/tmp/' . getenv('RSS_010_FILE'), FTP_ASCII);
+error_log('ftp_put : ' . $rc);
 
-error_log($rc);
+$rc = ftp_close($ftp_link_id);
+error_log('ftp_close : ' . $rc);
 
-ftp_close($ftp_link_id);
+
+$api_key = getenv('HEROKU_API_KEY');
+$url = 'https://api.heroku.com/account';
+
+$options = [
+    CURLOPT_URL => $url,
+    CURLOPT_USERAGENT => getenv('USER_AGENT'),
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_FOLLOWLOCATION => 1,
+    CURLOPT_MAXREDIRS => 3,
+    CURLOPT_SSL_FALSESTART => true,
+    CURLOPT_HTTPHEADER => [
+        'Accept: application/vnd.heroku+json; version=3',
+        "Authorization: Bearer ${api_key}",
+        ]
+];
+
+$ch = curl_init();
+curl_setopt_array($ch, $options);
+$res = curl_exec($ch);
+curl_close($ch);
+
+$data = json_decode($res, true);
+$account = explode('@', $data['email'])[0];
+
+$url = "https://api.heroku.com/accounts/${data['id']}/actions/get-quota";
+
+$options = [
+    CURLOPT_URL => $url,
+    CURLOPT_USERAGENT => getenv('USER_AGENT'),
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_FOLLOWLOCATION => 1,
+    CURLOPT_MAXREDIRS => 3,
+    CURLOPT_SSL_FALSESTART => true,
+    CURLOPT_HTTPHEADER => [
+        'Accept: application/vnd.heroku+json; version=3.account-quotas',
+        "Authorization: Bearer ${api_key}",
+        ]
+];
+
+$ch = curl_init();
+curl_setopt_array($ch, $options);
+$res = curl_exec($ch);
+curl_close($ch);
+
+$data = json_decode($res, true);
+
+$dyno_used = (int)$data['quota_used'];
+$dyno_quota = (int)$data['account_quota'];
+
+$tmp = $dyno_quota - $dyno_used;
+$tmp = floor($tmp / 86400) . 'd ' . ($tmp / 3600 % 24) . 'h ' . ($tmp / 60 % 60) . 'm';
 
 header('Content-Type: application/xml');
 
-echo '<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"/>';
+$xml_text = <<< __HEREDOC__
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+  <channel>
+    <title>quota</title>
+    <link>https://www.heroku.com/</link>
+    <description>none</description>
+    <language>ja</language>
+    <item><title>__TITLE__</title><link>https://www.heroku.com/</link><description>__DESCRIPTION__</description><pubDate/></item>
+  </channel>
+</rss>
+__HEREDOC__;
+
+$xml_text = str_replace('__TITLE__', $tmp, $xml_text);
+$xml_text = str_replace('__DESCRIPTION__', date('Y/m/d H:i', strtotime('+9 hours')), $xml_text);
+
+echo $xml_text;
 
 $time_finish = time();
 error_log("${pid} FINISH " . date('s', $time_finish - $time_start) . 's');
