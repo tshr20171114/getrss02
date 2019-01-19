0cf460a6345f3779b89b2c4532b317205e024385
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2019-01-20 05:41:33 +0900

    Update rss02.php

--- www/rss02.php
+++ www/rss02.php
@@ -44,7 +44,7 @@ $list_res = [];
 for ($j = 0; $j < 2; $j++) {
   $mh = curl_multi_init();
   $list_ch = [];
-  for ($i = 0; $i < 50; $i++) {
+  for ($i = 0; $i < 2; $i++) {
     $url = getenv('URL_020') . ($i + 1);
     if (array_key_exists($url, $list_res)) {
       continue;
