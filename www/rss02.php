09f59441d924567e56c8a3f3959cba18dc300c9b
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2019-01-19 12:56:47 +0900

    Update rss02.php

--- www/rss02.php
+++ www/rss02.php
@@ -116,9 +116,9 @@ foreach ($list_res as $res) {
   $tmp1 = explode('<hr />', $res);
   $list = explode('<div class="itemTitle">', $tmp1[0]);
   array_shift($list);
-  error_log(print_r($tmp1, true));
+  // error_log(print_r($tmp1, true));
   foreach ($list as $item) {
-    $rc = preg_match('/.+?<a href="(.+?)" title="(.+?)".+?<span class="movieTime">(.+?)<.+?<img src="(.+?)".+?<span class="proName".+?>(.+?)<.+?<span class="movieCnt".+?>(.+?)</s', $item, $match);
+    $rc = preg_match('/.+?<a href="(.+?)" title="(.+?)".+?<img src="(.+?)".+?<span class="movieTime">(.+?)<.+?<span class="proName".+?>(.+?)<.+?<span class="movieCnt".+?>(.+?)</s', $item, $match);
     error_log(print_r($match, true));
   }
 }
