c3bc5da0f6b19136a60019959455c6d0b2062487
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2019-01-20 06:17:44 +0900

    Update rss02.php

--- www/rss02.php
+++ www/rss02.php
@@ -162,13 +162,7 @@ $xml_root_text = <<< __HEREDOC__
 __HEREDOC__;
 
 $tmp = str_replace('__ITEMS__', implode("\r\n", $items), $xml_root_text);
-/*
-$tmp = str_replace('&hellip;', '', $tmp);
-$tmp = str_replace('&laquo;', '', $tmp);
-$tmp = str_replace('&raquo;', '', $tmp);
-$tmp = str_replace('&hearts;', '', $tmp);
-$tmp = str_replace('&rdquo;', '', $tmp);
-*/
+$tmp = str_replace('❗', '', $tmp);
 file_put_contents('/tmp/' . getenv('RSS_020_FILE'), $tmp);
 $rc = filesize('/tmp/' . getenv('RSS_020_FILE'));
 error_log('file size : ' . $rc);
