a7315d723d903e8f5a19cb6b6e693fb19f46fa74
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2019-01-19 09:27:54 +0900

    Update getcache01.php

--- www/getcache01.php
+++ www/getcache01.php
@@ -7,6 +7,37 @@ $cookie = $tmpfname = tempnam("/tmp", time());
 
 error_log(print_r(parse_url(getenv('URL_010')), true));
 
+$ch = curl_init();
+
+$options = [
+        CURLOPT_URL => 'https://' . parse_url(getenv('URL_010')['host'],
+        CURLOPT_USERAGENT => getenv('USER_AGENT'),
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => 'gzip, deflate, br',
+        CURLOPT_FOLLOWLOCATION => 1,
+        CURLOPT_MAXREDIRS => 3,
+        CURLOPT_HTTPHEADER => [
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+            'Cache-Control: no-cache',
+            'Connection: keep-alive',
+            'DNT: 1',
+            'Upgrade-Insecure-Requests: 1',
+            ],
+        CURLOPT_COOKIEJAR => $cookie,
+        CURLOPT_COOKIEFILE => $cookie,
+];
+  
+foreach ($options as $key => $value) {
+  $rc = curl_setopt($ch, $key, $value);
+}
+
+$res = curl_exec($ch);
+
+curl_close($ch);
+
+error_log(file_get_contents($cookie));
+
 for ($i = 0; $i < 40; $i++) {
   $url = getenv('URL_010') . ($i + 1);
   $file_name = '/tmp/' . hash('sha512', $url);
