9560b85732e6a2caf52770e3d1f766137992a18b
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2019-01-19 12:50:02 +0900

    Update rss02.php

--- www/rss02.php
+++ www/rss02.php
@@ -118,7 +118,7 @@ foreach ($list_res as $res) {
   array_shift($list);
   error_log(print_r($tmp1, true));
   foreach ($list as $item) {
-    $rc = preg_match('/<span class="proName".+?>(.+?)<.+?<span class="movieCnt">(.+?)<.+?<a href="(.+?)">.+?<div class="itemTitle">(.+?)<\/div>.+?<img src="(.+?)".+?<span class="movieTime">(.+?)</s', $item, $match);
+    $rc = preg_match('/.+?<a href="(.+?)" alt="(.+?)".+?<span class="movieTime">(.+?)<.+?<span class="proName".+?>(.+?)<.+?<span class="movieCnt".+?>(.+?)</s', $item, $match);
     error_log(print_r($match, true));
   }
 }
