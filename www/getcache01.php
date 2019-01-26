7b3f58753b453c58a461a9a900faa2c10faa2f78
tshr20171114 <33654977+tshr20171114@users.noreply.github.com>
2019-01-27 06:33:56 +0900

    Update getcache01.php

--- www/getcache01.php
+++ www/getcache01.php
@@ -12,10 +12,7 @@ $restart_flag = false;
 $loop_end_404[0] = 0;
 $loop_end_404[1] = 0;
 for ($j = 0; $j < 6; $j++) {
-  $loop_end = $j % 2 === 0 ? 170 : 20;
-  if ($loop_end_404[$j % 2] != 0) {
-    $loop_end = $loop_end_404[$j % 2] - 1;
-  }
+  $loop_end = $loop_end_404[$j % 2] === 0 ? 200 : $loop_end_404[$j % 2];
   for ($i = 0; $i < $loop_end; $i++) {
     if ($j % 2 === 0) {
       $url = getenv('URL_010') . ($i + 1);
